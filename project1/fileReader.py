a65f53efe0b2d49432861dea8dd6ddf7c34b0758
Knut Bjarte Haus <knutbjartehaus@dhcp-10-24-10-192.wlan.ntnu.no>
2018-02-08 16:59:14 +0100

    AdditionalFunctionsInfilereader

--- project1/fileReader.py
+++ project1/fileReader.py
@@ -1,4 +1,41 @@
 from random import randint
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
+import pylab as pl
+from matplotlib import collections  as mc
+
+def plot(solution, x,y,m,n,t):
+    totVehicles=m[0]*t[0]
+    color_list = plt.cm.Set3(np.linspace(0, 1, totVehicles))
+    fig, ax=pl.subplots()
+    plt.hold(True)
+    for i in range(0,totVehicles):
+        color=color_list[i]
+        depotIndex= n[0]+int(i/m[0])
+        segments=[]
+        if solution[i]:
+            segments.append([(x[depotIndex], y[depotIndex]), (x[solution[i][0]], y[solution[i][0]])])
+        l = len(solution[i])
+        this=0
+        next=0
+        for j in range(0,l-1):
+            this=solution[i][j]
+            next=solution[i][j+1]
+            segments.append([(x[this],y[this]), (x[next], y[next])])
+        segments.append([(x[next],y[next]), (x[depotIndex], y[depotIndex])])
+        lc=mc.LineCollection(segments, colors=color, linewidths=3)
+        ax.add_collection(lc)
+        ax.autoscale()
+        ax.margins(0.1)
+    plt.show()
+
+    
+
+
+
+
 
 
 
@@ -39,5 +76,7 @@ def main():
     n=[0]
     t=[0]
     reader('p01.txt',x,y,D,d,q,Q,m,n,t)
+    solution=genRandSol(m,n,t)
+    plot(solution,x,y,m,n,t)
 
 main()
