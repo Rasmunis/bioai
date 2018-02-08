fc3d6a8b43187cca704c49857de6059333986424
Knut Bjarte Haus <knutbjartehaus@dhcp-10-24-10-192.wlan.ntnu.no>
2018-02-08 14:04:41 +0100

    randomSolution

--- project1/fileReader.py
+++ project1/fileReader.py
@@ -1,21 +1,15 @@
-x=[]
-y=[]
-D=[]
-d=[]
-q=[]
-Q=0
-m=0
-n=0
-t=0
+from random import randint
 
-def reader(filename):
+
+
+def reader(filename,x,y,D,d,q,Q,m,n,t):
     file=open(filename,'r')
-    m,n,t = [int(i) for i in next(file).split()]
+    m[0],n[0],t[0] = [int(i) for i in next(file).split()]
     
-    for i in range(1,t+1):
+    for i in range(1,t[0]+1):
         array=next(file).split()
         D.append(int(array[0]))
-        Q=int(array[1])
+        Q[0]=int(array[1])
     for line in file:
         array=[int(i) for i in line.split()]
         x.append(array[1])
@@ -23,6 +17,27 @@ def reader(filename):
         d.append(array[3])
         q.append(array[4])
 
+def genRandSol(m,n,t):
+    totVehicles=m[0]*t[0]
+    solution = []
+    for i in range(0,totVehicles):
+        solution.append([])
+    for i in range (0,n[0]):
+        vehicle=randint(0,totVehicles-1)
+        solution[vehicle].append(i)
+    return solution
 
-reader('p01.txt')
 
+def main():
+    x=[]
+    y=[]
+    D=[]
+    d=[]
+    q=[]
+    Q=[0]
+    m=[0]
+    n=[0]
+    t=[0]
+    reader('p01.txt',x,y,D,d,q,Q,m,n,t)
+
+main()
---
+++ project1/solutionGenerator.py
@@ -0,0 +1,9 @@
+from random import randint
+
+def genRandSol():
+    totVehicles=m*t
+    solution = [none]*totVehicles
+    for i in range (0,n):
+        vehicle=randint(0,totVehicles)
+        solution[vehicle].appent(i))
+    print(solution)
