506794bae608b0e9695f7daa2b752c8ff00a27e6
Knut Bjarte Haus <knutbjartehaus@dhcp-10-24-9-61.wlan.ntnu.no>
2018-02-09 12:40:58 +0100

    fiksa plot og laga isValid

--- project1/fileReader.py
+++ project1/fileReader.py
@@ -29,7 +29,9 @@ def plot(solution, x,y,m,n,t):
             this=solution[i][j]
             next=solution[i][j+1]
             segments.append([(x[this],y[this]), (x[next], y[next])])
-        segments.append([(x[next],y[next]), (x[depotIndex], y[depotIndex])])
+        if solution[i]:
+            segments.append([(x[next],y[next]), (x[depotIndex], y[depotIndex])])
+        
         lc=mc.LineCollection(segments, colors=color, linewidths=3)
         ax.add_collection(lc)
         ax.autoscale()
@@ -79,6 +81,16 @@ def clusterSol(x,y,m,n,t):
         solution[k*m[0]+randint(0,m[0]-1)].append(i)
     return solution
 
+def isValid(sol,q,Q):
+    if Q==0:
+        return 1
+    for vehicle in sol:
+        demand=0
+        for customer in vehicle:
+                demand+=q[customer]
+        if demand>Q:
+            return 0
+    return 1
 
 def main(mutationRate, survivalProp, initPopulation, generations, crossoverRate):
     x=[]
@@ -92,12 +104,16 @@ def main(mutationRate, survivalProp, initPopulation, generations, crossoverRate)
     t=[0]
     reader('p01.txt',x,y,D,d,q,Q,m,n,t)
 #    plot(clusterSol(x,y,m,n,t),x,y,m,n,t)
-=======
 
     population = [clusterSol(x,y,m,n,t) for it in range(initPopulation)]
->>>>>>> 162d099576c9e18c6b5f6b9cb57fc9f73233b684
 
     population = [clusterSol(x,y,m,n,t) for it in range(initPopulation)]
+    for sol in population:
+        if (not (isValid(sol,q,Q))):
+            population.pop(sol)
+    
+
+    
     for i in range(generations):
         population.sort(key=lambda solution: fitness(solution, x, y, m, n, t))
         selection = population[:int(survivalProp*len(population))]
@@ -110,20 +126,15 @@ def main(mutationRate, survivalProp, initPopulation, generations, crossoverRate)
                 child = crossover(copy.deepcopy(selection[i % len(selection)]))
             else:
                 child = copy.deepcopy(selection[i % len(selection)])
-            population.append(child)
+            if(isValid(child,q,Q)):
+                population.append(child)
             i += 1
 
-    print(fitness(population[0],x,y,m,n,t))
-    plot(population[0],x,y,m,n,t)
-
-main(1, 0.2, 100, 100)
-=======
-            #print(fitness(population[0],x,y,m,n,t))
+    #print(fitness(population[0],x,y,m,n,t))
     population.sort(key=lambda solution: fitness(solution, x, y, m, n, t))
     print(fitness(population[0],x,y,m,n,t))
     plot(population[0], x, y, m, n, t)
 
 
-main(1, 0.1, 100, 5000, 0.7)
+main(1, 0.2, 200, 200,0)
 
->>>>>>> 162d099576c9e18c6b5f6b9cb57fc9f73233b684
