2b9d6cefd1d3c54198c6d3aa5e1e817bb8809950
Rasmus Stene <rasmunis@gmail.com>
2018-03-08 11:55:44 +0100

    reader and the framework of prims

--- project2/reader.go
+++ project2/reader.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/jpeg"
 	"os"
 )
 
 func main() {
-	file, _ := os.Open("./86016/testImg.jpg")
+	file, _ := os.Open("./86016/Test image.jpg")
 	img, _ := jpeg.Decode(file)
-
-	fmt.Println("Hello world", img.At(100, 100), img.Bounds())
+	r, g, b, _ := img.At(100, 100).NRGBA()
+	fmt.Println("Hello world", r, g, b, img.Bounds())
 }
+
+type Node struct {
+	X, Y       int
+	neighbours []*Node
+	root       *Node
+}
+
+func prims(img image.Image) {
+	// get image dimensions
+	// maxX := img.Bounds().Max.X
+	// maxY := img.Bounds().Max.Y
+
+	// // initialize mst
+	// mst := make([]byte, maxX*maxY)
+	//
+	// // keep track of which nodes are in the current mst
+	// nodesInMst := make([]int, 0)
+	//
+	// // generate random coordinate of initial mst-node
+	// randX := rand.Intn(maxX)
+	// randY := rand.Intn(maxY)
+	//
+	// // while the mst doesn't contain all pixels
+	// for len(nodesInMst) < maxX*maxY {
+	//
+	// 	// check all edges from mst and pick the smallest one
+	// 	for _, n := range nodesInMst {
+	//
+	// 	}
+	// }
+}
+
+func euclRGBdist() {}
