bb9ef1132e4e5b524c653f17aa5b23a3293cb67b
knutbh <knutbh@stud.ntnu.no>
2018-03-08 15:53:09 +0100

    backup git pull

---
+++ project2/evo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    /*"fmt"
+    "image"
+    "os"
+    "github.com/twmb/algoimpl/go/graph"*/
+
+)
+import _"image/jpeg"
+
+
+func main() {
+    
+    
+    P:=make([] *Solution,1,2)
+    F:=nonDominatedRank(P)
+    nextGen:= make([] *Solution, 1, 1)
+    for i:=0; i<N; i++{
+        r:=rand.New(rand.NewSource(time.Now().UnixNano()))
+        index = math.Pow(r.Float64,2)*N
+        child:=mutate(
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
